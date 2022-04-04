4fcaf67d88c92255605fb57e62517237913c1f49
Morphine <81608042+Morphine999@users.noreply.github.com>
2022-04-04 09:52:28 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Clash_Of_Clans_Information_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TownHall = new System.Windows.Forms.PictureBox();
+            this.League = new System.Windows.Forms.PictureBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TownHall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.League)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TownHall
+            // 
+            this.TownHall.Location = new System.Drawing.Point(260, 115);
+            this.TownHall.Name = "TownHall";
+            this.TownHall.Size = new System.Drawing.Size(74, 95);
+            this.TownHall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TownHall.TabIndex = 5;
+            this.TownHall.TabStop = false;
+            // 
+            // League
+            // 
+            this.League.Location = new System.Drawing.Point(340, 115);
+            this.League.Name = "League";
+            this.League.Size = new System.Drawing.Size(74, 95);
+            this.League.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.League.TabIndex = 4;
+            this.League.TabStop = false;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(12, 187);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(108, 23);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Search Player";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 25);
+            this.textBox1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox1.Location = new System.Drawing.Point(-46, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(618, 10);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(126, 187);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(128, 23);
+            this.metroButton2.TabIndex = 9;
+            this.metroButton2.Text = "Clan Search";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 216);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(402, 165);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(421, 398);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.League);
+            this.Controls.Add(this.TownHall);
+            this.Controls.Add(this.metroButton1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.ShowIcon = false;
+            this.Text = "Clash Of Clans Tool";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.TownHall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.League)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox TownHall;
+        private System.Windows.Forms.PictureBox League;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+using SharpOfClans;
+using SharpOfClans.Endpoints;
+using SharpOfClans.Models;
+using SharpOfClans.Paging;
+namespace Clash_Of_Clans_Information_Tool
+{
+    public partial class Form1 : MetroForm
+    {
+        private const string _TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjA0MjE4NjNiLTkxMWItNDQzOC1hMDE4LTc0MmNjMjg2NzkxOSIsImlhdCI6MTY0OTA0NTYyMCwic3ViIjoiZGV2ZWxvcGVyL2ZiMGQ4YmY1LTdhMDktYjllZS01MTZjLTEyYWFkM2ExZjgzNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk5LjI1Mi4xMjIuMTI4Il0sInR5cGUiOiJjbGllbnQifV19.h-QQWiNBV2f7QDI0n1dckIh5FFiAMInrGM4tbDjT3_OkoyrXIfCdV_Yo2jqARd_VNtDeX5fKwTzYh6owga3eLg";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void metroButton1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            var clashApi = ClashApi.GetInstance(_TOKEN);
+            try
+            {
+                var Player = await clashApi.Players.GetPlayerAsync(textBox1.Text);
+                richTextBox1.AppendText($"Username: {Player.Name}");
+                richTextBox1.AppendText($"\nTag: {Player.Tag}");
+                richTextBox1.AppendText($"\nLeague: {Player.League.Name}");
+                richTextBox1.AppendText($"\nTrophies: {Player.Trophies}");
+                richTextBox1.AppendText($"\nBest Trophies{Player.BestTrophies}");
+                richTextBox1.AppendText($"\nTownhall Level: {Player.TownHallLevel}");
+                richTextBox1.AppendText($"\nWar Stars: {Player.WarStars}");
+                richTextBox1.AppendText($"\nAttack Wins: {Player.AttackWins}");
+                richTextBox1.AppendText($"\nComlpeted Achievements: {Player.Achievements.Count}");
+                richTextBox1.AppendText($"\nTotal Donations: {Player.Donations}");
+                if (Player.League.Name == "Bronze League I" || Player.League.Name == "Bronze League II" || Player.League.Name == "Bronze League III")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                else if (Player.League.Name == "Silver League I" || Player.League.Name == "Silver League II" || Player.League.Name == "Silver League III")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                else if (Player.League.Name == "Gold League I" || Player.League.Name == "Gold League II" || Player.League.Name == "Gold League III")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                else if (Player.League.Name == "Crystal League I" || Player.League.Name == "Crystal League II" || Player.League.Name == "Crystal League III")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                else if (Player.League.Name == "Master League I" || Player.League.Name == "Master League II" || Player.League.Name == "Master League III")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                else if (Player.League.Name == "Champion League I" || Player.League.Name == "Champion League II" || Player.League.Name == "Champion League III")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                else if (Player.League.Name == "Titan League I" || Player.League.Name == "Titan League II" || Player.League.Name == "Titan League III")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                else if (Player.League.Name == "Legend League")
+                {
+                    League.Load(Player.League.IconUrls.Medium);
+                }
+                if(Player.TownHallLevel == 1)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942212820232196146/100.png");
+                }else if(Player.TownHallLevel == 2)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942212966525304862/100.png");
+                }else if(Player.TownHallLevel == 3)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942212999538679880/100.png");
+                }else if(Player.TownHallLevel == 4)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213018404651048/100.png");
+                }else if(Player.TownHallLevel == 5)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213039351013416/100.png");
+                }else if(Player.TownHallLevel == 6)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213059513024512/100.png");
+                }else if(Player.TownHallLevel == 7)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213078978797598/100.png");
+                }
+                else if (Player.TownHallLevel == 8)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213110767443998/100.png");
+                }
+                else if (Player.TownHallLevel == 9)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213189960081418/100.png");
+                }
+                else if (Player.TownHallLevel == 10)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213202635268146/115.png");
+                }
+                else if (Player.TownHallLevel == 11)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213215847325756/110.png");
+                }
+                else if (Player.TownHallLevel == 12)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213231131385876/120.png");
+                }
+                else if (Player.TownHallLevel == 13)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213249175273532/120.png");
+                }
+                else if (Player.TownHallLevel == 14)
+                {
+                    TownHall.Load("https://cdn.discordapp.com/attachments/880597112767774761/942213258306261012/110.png");
+                }
+                if(Player.TownHallLevel < 1)
+                {
+                    TownHall.Load("");
+                }
+            }
+            catch (ClashApiException ex)
+            {
+                richTextBox1.Clear();
+                MessageBox.Show($"That user either doesn't exist or the ID provided is incorrect.\n\n{ex.Message}", "FATAL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                TownHall.Load("");
+                League.Load("");
+                return;
+            }
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            richTextBox1.ReadOnly = true;
+        }
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This feature is coming soon.", "Soon", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
